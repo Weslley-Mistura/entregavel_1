85d5630fd6cd0b86877b6d85c6fd593f47303034
Weslley Mistura <weslleymisturadev@gmail.com>
2025-03-27 21:34:02 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,32 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Calculadora(numero1, numero2, operador){
+  let resultado 
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+  
 
-//ESCREVA SEU CÓDIGO AQUI!!
+  switch(operador){
+      case '+':
+          resultado = num1 + num2;
+          break;
+      case '-':
+          resultado = num1 - num2;
+          break;
+      case "/":
+          resultado = num1 / num2;
+          break;
+      case '*':
+          resultado = num1 * num2;
+          break;
+      case 'e':
+          resultado = num1 ** num2;
+          break; 
+  }
 
+  if(resultado == undefined || resultado > 1000000)
+    resultado = "ERRO"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  return resultado
+}
 
 let visor = document.getElementById("visor");
 
